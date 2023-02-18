11fe73ed72c6c635ce8def17cada04177b161fd8
Pranav Puranam <pranav.puranam@gmail.com>
2023-02-18 01:24:35 -0800

    switch around methods

--- backend/utils.py
+++ backend/utils.py
@@ -1,9 +1,12 @@
 ###### HELPER FUNCTIONS ######
 
 def generate_poll_prompt(transcript: str) -> str:
-    prompt = "Create 3 question poll using: " + transcript
-    return prompt
+    pass
 
 def generate_summary_prompt(transcript: str) -> str:
     prompt = "Create a few bullet points summary on: " + transcript
+    return prompt
+
+def generate_quiz_prompt(transcript: str) -> str:
+    prompt = "Create 3 question quiz using: " + transcript
     return prompt
