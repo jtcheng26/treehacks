7dd124a6d630629d25dc42c8de1556674516a6f2
Bach Ngo <bachngo@Bachs-MacBook-Pro-1390.local>
2023-02-18 23:44:31 -0800

    display changes

--- src/components/buttons/EnterMeetingCode.jsx
+++ src/components/buttons/EnterMeetingCode.jsx
@@ -4,6 +4,7 @@ import CustomMeetingButton from "./CustomMeetingButton";
 export default function EnterMeetingCode() {
     return (
         <div style={{float: 'left'}}>
+<<<<<<< Updated upstream
             <form label=''>
                 <input 
                 className="font-sans"
@@ -12,6 +13,20 @@ export default function EnterMeetingCode() {
                 >
                 </input>
                 <CustomMeetingButton/>
+=======
+            <form label='' className="flex flex-row items-center">
+                <div>
+                    <input 
+                    className="font-sans"
+                    style={{fontSize: '2.5vmin', display: 'inline-flex', justifyContent: "center", width: '23vw', height: '7vh', marginLeft: '3vw', border: 'double .7vh transparent', backgroundImage: 'linear-gradient(white, white), radial-gradient(circle at top left, #10B981, #6366F1)', backgroundOrigin: 'border-box', backgroundClip: 'content-box, border-box', textAlign: 'left', textIndent: '2vw', borderRadius: '50px', marginTop: '2vh'}}
+                    placeholder='enter a meeting code'
+                    >
+                    </input>
+                </div>
+                <div>
+                    <CustomMeetingButton/>
+                </div>
+>>>>>>> Stashed changes
             </form>
         </div>
     );
